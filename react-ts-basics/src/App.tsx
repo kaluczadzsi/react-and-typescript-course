92e1c43e68035bbd5cd12df452ce4deea81590fc
Gábor Kalucza <kaluczadzsi@gmail.com>
2023-12-31 11:47:49 +0100

    using typescript with react

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+import CourseGoal from './components/CourseGoal';
+import CourseGoalList from './components/CourseGoalList';
+import NewGoal from './components/NewGoal';
+import Header from './components/Header';
+import goalsImg from './assets/goals.jpg';
+
+export type CourseGoal = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(goal: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
+}
